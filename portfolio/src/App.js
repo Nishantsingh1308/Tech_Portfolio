d1bb1ed3e146890370bb2e97a025f792e8ba3d6b
Nishantsingh1308 <nishantsinghpundhir143@gmail.com>
2025-02-22 23:03:29 +0530

    .

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/skills';
+import Services from './components/services/Services';
 // import Qualification from './components/qualification/Qualification';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Services />
       {/* <Qualification /> */}
     </main>
     </>
---
+++ portfolio/src/components/services/Services.jsx
@@ -0,0 +1,179 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+        <h2 className="section__title">Services</h2>
+        <span className="section__subtitle">What i do</span>
+
+        <div className="services__container container grid">
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-web-grid services__icon"></i>
+                    <h3 className="services__title">Product <br /> Designer </h3>
+                </div>
+
+                <span className="services__button">
+                    View More{" "} <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">
+                            Work experience more than 6 months. 
+                            doing quality work and contributing with friends.
+                        </p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web Page Development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I create UX element interaction.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Contribute and participate with teams.
+                                </p>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                </div>
+
+                <span className="services__button">
+                    View More{""} <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">
+                            Work experience more than 6 months. 
+                            doing quality work and contributing with friends.
+                        </p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web Page Development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I create UX element interaction.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Contribute and participate with teams.
+                                </p>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil services__icon"></i>
+                    <h3 className="services__title"></h3>
+                </div>
+
+                <span className="services__button">
+                    View More {""}<i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">
+                            Work experience more than 6 months. 
+                            doing quality work and contributing with friends.
+                        </p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I develop user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Web Page Development.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    I create UX element interaction.
+                                </p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle service__modal-icon"></i>
+                                <p className="services__modal-info">
+                                    Contribute and participate with teams.
+                                </p>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
